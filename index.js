deaec6a9c13b276cc77c33ffcdf5c35a32da45c6
VladChaka <super.chuhunov@yandex.com>
2018-06-05 22:49:18 +0300

    add rest api

--- index.js
+++ index.js
@@ -2,38 +2,25 @@ let getParam = require("./Util/common"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser"),
     express = require("express"),
-    rout = require("./modules/users"),
+    rout = require("./api/models/model"),
     port = getParam("port", 4000),
-    app = express();
-    let user = require("./modules/users/index"),
+    app = express(),
     database;
 
-mongoose.connect("mongodb://admin:vlad12345@ds245170.mlab.com:45170/mydb", function (err, db) {
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://admin:vlad12345@ds245170.mlab.com:45170/mydb", function(err, db) {
     if (err) console.log("Error");
-
     database = db;
     console.log("Success db connection");
 });
 
-app.get("/", function (req, res) {
-
-    database.collection("users").find({}).toArray(function(err, user){
-        if (err) console.log("Error");
-        res.render("index.hbs", {
-            user
-        });
-        
-    });
-});
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended: false } ));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
 
-// ROUTING
-app.use('/', require('./modules/users/index'));
-// END ROUTING
+var routes = require('./api/routes/route'); //importing route
+routes(app); //register the route
 
 app.listen(port, () => {
     console.log(`Start server on ${port} port`);

a44b53d94120cb5d971e278e8eb7f2d6b1698e10
VladChaka <super.chuhunov@yandex.com>
2018-06-07 20:33:30 +0300

    Fix bugs

--- modules/users/index.js
+++ modules/users/index.js
@@ -1,10 +1,10 @@
 require('../../models/user');
 const express = require('express')
-      mongoose = require("mongoose"),
-      fs = require("fs"),
-      User = mongoose.model('User'),
-      app = express(),
-      router = express.Router();
+mongoose = require("mongoose"),
+    fs = require("fs"),
+    User = mongoose.model('User'),
+    app = express(),
+    router = express.Router();
 
 function writeInDb() {
     User.find({}, function(err, users) {
@@ -13,114 +13,113 @@ function writeInDb() {
         file = users;
         fs.writeFileSync(__dirname + '/public/database.json', JSON.stringify(file, null, 2));
     });
-}  
+}
 
-// router.get('/', (req, res) => {  
-//     //res.sendFile(__dirname + '/index.html');  
-    
-// });
-
-router.post('/user/add', (req, res) => {  
-	handlerMethod(req, res);  
+router.post('/user/add', (req, res) => {
+    handlerMethod(req, res);
 });
 
 router.post('/user/update', (req, res) => {
-	handlerMethod(req, res)
-});
-
-router.post('/user/delete', (req, res) => {	
     handlerMethod(req, res)
 });
 
-function handlerMethod(req ,res) {
-	console.log(req.body);
-	
-	let date = new Date;
-	const id = req.body.id,
-		  username = req.body.username || "",
-		  email = req.body.email || "",
-		  phone = req.body.phone || "",
-		  pass = req.body.password || "",
-		  fullname = req.body.fullname || "",
-		  admin = (req.body.administrator === "on") ? "Administrator" : "",
-		  frontend = (req.body.frontend === "on") ? "Frontend" : "",
-		  backend = (req.body.backend === "on") ? "Backend" : "",
-		  moderator = (req.body.moderator === "on") ? "Moderator" : "",
-		  redactor = (req.body.redactor === "on") ? "Redactor" : "",
-		  visitor = (req.body.visitor === "on") ? "Visitor" : "",
-		  regDate = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear(),
-		  post = admin + " " + frontend + " " + backend + " " + moderator + " " + redactor + " " + visitor;
+router.post('/user/delete', (req, res) => {
+    handlerMethod(req, res)
+});
 
-			console.log("username",email);
-	if (!checkRegExEmail(email)) return res.json({ error: "Incorrect email" });
-	if (!checkRegExLogin(username)) return res.json({ error: "Incorrect login" });
-	if (pass.length < 8) return res.json({ error: "Incorrect password. Min 8 simbols." });
+function handlerMethod(req, res) {
+    console.log(req.body);
 
-	if (req.body._method === "POST") {	
-		const new_user = new User({
-			username: username,
-			email: email,
-			post: post,
-			phone: phone,
-			password: pass,
-			fullname: fullname,
-			rating: 0,
-			regDate: regDate
-	  });
-	  
-	  new_user.save(function(err, user) {
-		  if (err) return res.json({ error: "Duplicate username or email" });
-		  writeInDb();
-		  res.json({ success: "Success add user" });
-	  });
-	} else if (req.body._method === "PUT" || req.body._method[0] === "PUT") {
-		console.log(req.body);
-		
-		// id = req.body.id;
-		// username = req.body.username || "";
-		// email = req.body.email || "";
-		// phone = req.body.phone || "";
-		// pass = req.body.password || "";
-		// fullname = req.body.fullname || "";
-		// admin = (req.body.administrator === "") ? "Administrator" : "";
-		// frontend = (req.body.frontend === "") ? "Frintend" : "";
-		// backend = (req.body.backend === "") ? "Backend" : "";
-		// moderator = (req.body.moderator === "") ? "Moderator" : "";
-		// redactor = (req.body.redactor === "") ? "Redactor" : "";
-		// visitor = (req.body.visitor === "") ? "Visitor" : "";
-		// post = admin + " " + frontend + " " + backend + " " + moderator + " " + redactor + " " + visitor;	
+    let date = new Date;
+    const method = req.body._method,
+        id = req.body.id,
+        username = req.body.username || "",
+        email = req.body.email || "",
+        phone = req.body.phone || "",
+        pass = req.body.password || "",
+        fullname = req.body.fullname || "",
+        admin = (req.body.administrator === "on") ? "Administrator" : "",
+        frontend = (req.body.frontend === "on") ? "Frontend" : "",
+        backend = (req.body.backend === "on") ? "Backend" : "",
+        moderator = (req.body.moderator === "on") ? "Moderator" : "",
+        redactor = (req.body.redactor === "on") ? "Redactor" : "",
+        visitor = (req.body.visitor === "on") ? "Visitor" : "",
+        regDate = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear(),
+        post = admin + " " + frontend + " " + backend + " " + moderator + " " + redactor + " " + visitor;
 
-		User.findOneAndUpdate({ _id: id }, {
-			username: username,
-			email: email,
-			post: post,
-			phone: phone,
-			password: pass,
-			fullname: fullname
-		}, 
-		{ new: true },
-		function(err, user) {
-			if (err) res.send(err);
-			writeInDb();
-			res.json({ success: "Success update user" });
-		});
-	} else if (req.body._method === "DELETE") {
-		let id = req.body.id;
-		User.remove({ _id: id }, function(err, user) {
-			if (err) res.send(err);
-			writeInDb();
-			res.json({ success: "Success delete user" });
-		}); 
-	}
+    if (method === "POST") {
+        if (!checkRegExEmail(email)) return res.json({ error: "Incorrect email" });
+        if (!checkRegExLogin(username)) return res.json({ error: "Incorrect login" });
+        if (pass.length < 8) return res.json({ error: "Incorrect password. Min 8 simbols." });
+
+        const new_user = new User({
+            username: username,
+            email: email,
+            post: post,
+            phone: phone,
+            password: pass,
+            fullname: fullname,
+            rating: 0,
+            regDate: regDate
+        });
+
+        new_user.save(function(err, user) {
+            if (err) return res.json({ error: "Duplicate username or email" });
+            writeInDb();
+            res.json({ success: "Success add user" });
+        });
+    } else if (method === "PUT") {
+        console.log("body", req.body);
+        if (!checkRegExEmail(email)) return res.json({ error: "Incorrect email" });
+        if (!checkRegExLogin(username)) return res.json({ error: "Incorrect login" });
+        if (pass.length < 8) return res.json({ error: "Incorrect password. Min 8 simbols." });
+
+        // id = req.body.id;
+        // username = req.body.username || "";
+        // email = req.body.email || "";
+        // phone = req.body.phone || "";
+        // pass = req.body.password || "";
+        // fullname = req.body.fullname || "";
+        // admin = (req.body.administrator === "") ? "Administrator" : "";
+        // frontend = (req.body.frontend === "") ? "Frintend" : "";
+        // backend = (req.body.backend === "") ? "Backend" : "";
+        // moderator = (req.body.moderator === "") ? "Moderator" : "";
+        // redactor = (req.body.redactor === "") ? "Redactor" : "";
+        // visitor = (req.body.visitor === "") ? "Visitor" : "";
+        // post = admin + " " + frontend + " " + backend + " " + moderator + " " + redactor + " " + visitor;	
+
+        User.findOneAndUpdate({ _id: id }, {
+                username: username,
+                email: email,
+                post: post,
+                phone: phone,
+                password: pass,
+                fullname: fullname
+            }, { new: true },
+            function(err, user) {
+                if (err) res.send(err);
+                writeInDb();
+                res.json({ success: "Success update user" });
+            });
+    } else if (method === "DELETE") {
+        console.log(req.body.id);
+
+        let id = req.body.id;
+        User.remove({ _id: id }, function(err, user) {
+            if (err) res.send(err);
+            writeInDb();
+            res.json({ success: "Success delete user" });
+        });
+    }
 }
 
 // CUSTOM FUNCTIONS
 function checkRegExLogin(login) {
-	return /^[a-zA-Z1-9]+$/.test(login) && login.length > 3 && login.length < 15;
+    return /^[a-zA-Z1-9]+$/.test(login) && login.length > 3 && login.length < 15;
 }
-function checkRegExEmail(email)
-{
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+
+function checkRegExEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 //
 
--- modules/users/public/js/users.js
+++ modules/users/public/js/users.js
@@ -1,26 +1,25 @@
-
 var usersApp = angular.module("usersApp", []);
 
-usersApp.controller('usersController', function($scope,$http) {
-    $scope.addElement = function(element){
+usersApp.controller('usersController', function($scope, $http) {
+    $scope.addElement = function(element) {
         var cell = document.getElementById(element).content.cloneNode(true);
         addedTemplates.appendChild(cell);
     };
 
-    $http.get('database.json').then( function(response) {
+    $http.get('database.json').then(function(response) {
         $scope.users = {
             info: response.data
         };
     });
     $scope.currentPage = 0;
-    $scope.pageSize=15;
-    $scope.numberOfPages=function () {
-        return Math.ceil($scope.users.info.length/$scope.pageSize);
+    $scope.pageSize = 15;
+    $scope.numberOfPages = function() {
+        return Math.ceil($scope.users.info.length / $scope.pageSize);
     }
 
 });
-usersApp.filter('startFrom', function () {
-    return function (input, start) {
+usersApp.filter('startFrom', function() {
+    return function(input, start) {
         start = +start;
         return input.slice(start);
     }
@@ -34,6 +33,7 @@ function show(element, id) {
         document.body.style.paddingRight = '15px';
     };
 };
+
 function showEditForm(ths) {
     var y = ths.parentNode.parentNode;
     y.getElementsByClassName('user-profile-container')[0].style.display = "flex";
@@ -42,6 +42,7 @@ function showEditForm(ths) {
         document.body.style.paddingRight = '15px';
     };
 };
+
 function hideEditForm(ths) {
     var y = ths.parentNode.parentNode.parentNode;
     y.getElementsByClassName('user-profile-container')[0].style.display = "none";
@@ -50,6 +51,7 @@ function hideEditForm(ths) {
         document.body.style.paddingRight = '0';
     };
 };
+
 function hide(element) {
     element.parentNode.style.display = "none";
     document.getElementById("popupsContainer").style.display = "none";
@@ -58,65 +60,64 @@ function hide(element) {
 }
 
 document.getElementById("orderPopup").style.display = "none";
+
 function showHideOrders() {
     var toggle = document.getElementById("orderPopup").style.display;
     if (toggle == "none") {
         document.getElementById("orderPopup").style.display = "flex";
-    } else{
+    } else {
         document.getElementById("orderPopup").style.display = "none";
     }
 }
 var activeFiltler = document.getElementsByClassName('filters-order-active');
+
 function filterByRating() {
-   var rating = document.getElementsByClassName('list-user-rating');
-   var date = document.getElementsByClassName('list-user-date');
-   for (i=0;i<date.length;i++){
-       date[i].style.display = "none";
-   }
-   for (i=0;i<rating.length;i++){
+    var rating = document.getElementsByClassName('list-user-rating');
+    var date = document.getElementsByClassName('list-user-date');
+    for (i = 0; i < date.length; i++) {
+        date[i].style.display = "none";
+    }
+    for (i = 0; i < rating.length; i++) {
         rating[i].style.display = "block";
-   }
-   activeFiltler[0].innerHTML ="By Rating";
+    }
+    activeFiltler[0].innerHTML = "By Rating";
     showHideOrders();
 }
+
 function deleteUser(user) {
-	var id = user.parentNode.name;	
-	id = "#"+id+"/";
-	console.log(id);
-	
-	
-	$.post('http://localhost:4001/user/delete', $(id).serialize(), function (response) {
-		console.log(response);
-	});
+    var id = "." + user.parentNode.name;
+    $.post('http://localhost:4001/user/delete', $(id).serialize(), function(response) {
+        console.log(response);
+    });
     var x = user.parentNode.parentNode.parentNode.style.display = "none";
 }
+
 function filterByDate() {
     var rating = document.getElementsByClassName('list-user-rating');
     var date = document.getElementsByClassName('list-user-date');
-    for (i=0;i<rating.length;i++){
+    for (i = 0; i < rating.length; i++) {
         rating[i].style.display = "none";
     }
-    for (i=0;i<date.length;i++){
+    for (i = 0; i < date.length; i++) {
         date[i].style.display = "block";
     }
-    activeFiltler[0].innerHTML ="By Date";
+    activeFiltler[0].innerHTML = "By Date";
     showHideOrders();
 }
+
 function addUser() {
-	$.post('http://localhost:4001/user/add', $('#add').serialize(), function (response) {
-		console.log(response);
-	});
-	// $(document).ready(function(){  
-	// 	setInterval(show,1000);  
-	// });
+    $.post('http://localhost:4001/user/add', $('#add').serialize(), function(response) {
+        console.log(response);
+    });
+    // $(document).ready(function(){  
+    // 	setInterval(show,1000);  
+    // });
 }
 
 function updateUser(param) {
-	console.log(param);
-    var id = param.parentNode.parentNode.id.value;
-	$.post('http://localhost:4001/user/update', $('#'+id).serialize(), function (response) {
-		//console.log(response);
-	});
+    console.log(param);
+    var id = "#" + param.parentNode.parentNode.id.value;
+    $.post('http://localhost:4001/user/update', $(id).serialize(), function(response) {
+        console.log(response);
+    });
 }
-
-  

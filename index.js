f7c8762d14105c7e6af9c3067b58d643c20bef5f
VladChaka <super.chuhunov@yandex.com>
2018-06-10 21:20:41 +0300

    Optimized code

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ let express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose");
 
-mongoose.connect("mongodb://admin:vlad12345@ds245170.mlab.com:45170/mydb", function (err) {
+mongoose.connect("mongodb://admin:vlad12345@ds245170.mlab.com:45170/mydb", function(err) {
     if (err) {
-		console.log(err);
-	} else {
-		console.log("Success db connection");
-	}  
+        console.log(err);
+    } else {
+        console.log("Success db connection");
+    }
 });
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended: false } ));
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/node_modules/public_chameleon47'));
 
 //route
 app.use('/', require(__dirname + '/modules/index'));
